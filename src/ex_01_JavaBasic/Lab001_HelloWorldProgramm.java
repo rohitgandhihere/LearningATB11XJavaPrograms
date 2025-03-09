5cdb04335a844e59a5a30060088135e553c2e87e
ROHITGANDHI32 <ROHITGANDHI32>
2025-03-09 14:24:56 +0530

    7 March'25

---
+++ src/ex_01_JavaBasic/Lab001_HelloWorldProgramm.java
@@ -0,0 +1,9 @@
+package ex_01_JavaBasic;
+
+public class Lab001_HelloWorldProgramm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World program!");
+    }
+
+}
