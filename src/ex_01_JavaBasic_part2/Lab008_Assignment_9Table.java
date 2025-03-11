67e63d8dfe85122b0ad77830206808edc4a088da
ROHITGANDHI32 <ROHITGANDHI32>
2025-03-11 08:15:53 +0530

    11 March - Assignment

---
+++ src/ex_01_JavaBasic_part2/Lab008_Assignment_9Table.java
@@ -0,0 +1,33 @@
+package ex_01_JavaBasic_part2;
+
+public class Lab008_Assignment_9Table {
+    public static void main(String[] args) {
+
+        int i=9, count=1;
+        System.out.println("Table of 9 is as follows");
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+        count++;
+        System.out.printf("%d * %d = %d ",i, count, (i*count));
+
+
+    }
+
+
+}
