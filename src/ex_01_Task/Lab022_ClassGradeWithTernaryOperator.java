aef7e20c5b5c16b00d8f4b673604c6897db06e1c
ROHITGANDHI32 <ROHITGANDHI32>
2025-03-13 23:31:05 +0530

    13 March -Assignment

---
+++ src/ex_01_Task/Lab022_ClassGradeWithTernaryOperator.java
@@ -0,0 +1,17 @@
+package ex_01_Task;
+
+public class Lab022_ClassGradeWithTernaryOperator {
+
+    public static void main(String[] args) {
+
+//        A: 90-100  B: 80-89  C: 70-79 D: 60-69  E: 0-59
+
+       // String result = (age < 18) ? "Minor" : (age < 65) ? "Adult" : "Senior";
+        //int marks= 61;
+        String marks1 = args[0];
+        Integer marks = Integer.parseInt(marks1);
+        String result = (marks>90) ? "A" : (marks>80)? "B":(marks>70)?"C":(marks>60)?"D":"E";
+
+        System.out.println("Result = "+result);
+    }
+}
